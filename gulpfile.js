a5292548fe7b4296c5f6bc1a46fba89599b326cf
nguyen manh <nguyenmanh1507@gmail.com>
2015-10-11 18:51:36 +0700

    install & test gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,33 @@
-var gulp = require('gulp')
+'use strict';
 
+var gulp = require('gulp'),
+		sass = require('gulp-sass'),
+		rename = require('gulp-rename'),
+		sourcemaps = require('gulp-sourcemaps'),
+		wiredep = require('wiredep').stream
 ;
+
+// Scss task
+gulp.task('sass', function() {
+	return gulp.src('./app/scss/app.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass({
+			outputStyle: 'compressed'
+		}).on('error', sass.logError))
+		.pipe(sourcemaps.write())
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest('./app/css'))
+		;
+});
+
+// Bower task
+gulp.task('bower', function() {
+	return gulp.src('./app/*.html')
+		.pipe(wiredep({
+			exclude: ['./app/bower_components/modernizr/dist/modernizr-build.js']
+		}))
+		.pipe(gulp.dest('./app'))
+		;
+});
